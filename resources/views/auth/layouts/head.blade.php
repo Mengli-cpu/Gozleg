a880a577134d37641d991f108c6185e140adc612
Menli <mengli.ashyrmedova@gmail.com>
2026-05-15 10:07:18 +0500

    7 Commit

--- resources/views/auth/layouts/head.blade.php
+++ resources/views/auth/layouts/head.blade.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gozleg</title>
+    <title>Admin Panel</title>
     
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/bootstrap-icons.min.css') }}">
--- resources/views/client/layouts/head.blade.php
+++ resources/views/client/layouts/head.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin panele</title>
+    <title>Gozleg</title>
     
     <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/bootstrap-icons.min.css') }}">
